4613ea68d8e6f7f9b1cec068f27d28eea889d979
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-03-26 19:09:28 +0000

    chore: mark author and files for Denys

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,3 +1,7 @@
+// Author: Denys(Ezpectus)
+// Files: prisma.ts, env.ts, auth.middleware.ts, auth.controller.ts, video.controller.ts, comment.controller.ts
+
+
 import dotenv from 'dotenv'
 dotenv.config()
 
---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,2 @@
+// Author: Denys(Ezpectus)
+// Files: prisma.ts, env.ts, auth.middleware.ts, auth.controller.ts, video.controller.ts, comment.controller.ts
