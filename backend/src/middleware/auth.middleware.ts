01cbba33efdafee9670a8d8adcf198d6b02e5fa6
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-03-26 20:42:47 +0000

    add auth middleware base and AuthRequest intf

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,2 +1,15 @@
 // Author: Denys(Ezpectus)
 // Files: prisma.ts, env.ts, auth.middleware.ts, auth.controller.ts, video.controller.ts, comment.controller.ts
+
+
+import {Request, Response, NextFunction} from 'express'
+import jwt from 'jsonwebtoken'
+import {ENV } from '../config/env'
+
+
+interface AuthRequest extends Request{
+    userId?: string
+}
+
+//authMiddleware
+
