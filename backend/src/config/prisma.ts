ce128cd51f4d084e718b750748d07fee335befc0
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-03-26 20:14:09 +0000

    feat: add prisma client singleton

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,2 +1,13 @@
 // Author: Denys(Ezpectus)
 // Files: prisma.ts, env.ts, auth.middleware.ts, auth.controller.ts, video.controller.ts, comment.controller.ts
+
+
+const { PrismaClient } = require("@prisma/client")
+
+declare global {
+    var prisma: InstanceType<typeof PrismaClient> | undefined
+}
+
+export const prisma = global.prisma  || new PrismaClient()
+if(process.env.ENV !== 'production') global.prisma = prisma
+
