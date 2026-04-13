a66994f90109b4670c725d8c693243d1981894b6
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-04-13 11:11:24 +0100

    feat: upd auth controller

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,34 +1,31 @@
 // Author: Denys(Ezpectus)
 // Files: prisma.ts, env.ts, auth.middleware.ts, auth.controller.ts, video.controller.ts, comment.controller.ts
-
 import { Request, Response } from "express";
 import { authService } from "../services/auth.service";
 
 export const authController = {
-    async register(req:Request , res: Response){
+    async register(req: Request, res: Response) {
         try {
-        const {email,password} = req.body;
-        if(!email || !password) return res.status(400).json({ message : " Missing fileds"});
-        
-        const result = await authService.register(email,password);
-        return res.json(result);
-   
-             } catch(error) {
-               return res.status(500).json({ message: "Server error" });
-          }
-        },
-
-
-     async login(req: Request, res: Response) { 
-        try {
-         const{ email,password} = req.body;
-        if(!email || !password)  return res.status(400).json({message : "Missing something"});
-
-        const result  = await authService.login(email,password);
-        return res.json(result);
-
-          } catch(error) {
+            const { email, password, username } = req.body;
+            if (!email || !password || !username) 
+                return res.status(400).json({ message: "Missing fields" });
+            
+            const result = await authService.register(email, password, username);
+            return res.json(result);
+        } catch (error) {
             return res.status(500).json({ message: "Server error" });
-       }
+        }
+    },
+    async login(req: Request, res: Response) {
+        try {
+            const { email, password } = req.body;
+            if (!email || !password) 
+                return res.status(400).json({ message: "Missing fields" });
+            
+            const result = await authService.login(email, password);
+            return res.json(result);
+        } catch (error) {
+            return res.status(500).json({ message: "Server error" });
+        }
     }
 }
