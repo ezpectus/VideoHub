1c817d1ba81d2624d5af13e774dca3e55d431614
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-04-20 18:23:31 +0100

    feat:updated google OAth

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,6 +1,5 @@
 // Author: Denys(Ezpectus)
 // Files: prisma.ts, env.ts, auth.middleware.ts, auth.controller.ts, video.controller.ts, comment.controller.ts
-
 import dotenv from 'dotenv';
 dotenv.config();
 
