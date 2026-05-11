f5b1932b5c04a065ac213c2208b58b004ee11898
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-05-11 11:33:02 +0100

    fix: added userID

--- backend/src/controllers/subscription.controller.ts
+++ backend/src/controllers/subscription.controller.ts
@@ -1,48 +1,49 @@
 // Author Jutsu78 (Oleksii)
 
-import { Request, Response } from 'express';
+import { Response } from 'express';
+import { AuthRequest } from '../middleware/auth.middleware';
 import { prisma } from '../config/prisma';
 
 class SubscribeController {
-    async toggleSubscription(req: any, res: Response) {
-try {
-    const { channelId } = req.params;
-    const subscriberId = req.userId;
+  async toggleSubscription(req: AuthRequest, res: Response) {
+    try {
+      const { channelId } = req.params;
+      const subscriberId = req.userId;
 
-     if (subscriberId === channelId) {
+      
+      if (!subscriberId) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      if (subscriberId === channelId) {
         return res.status(400).json({ message: "You cannot subscribe to yourself." });
-    }
+      }
 
-    const existingSubscription = await prisma.subscription.findUnique({
+      const existingSubscription = await prisma.subscription.findUnique({
         where: {
-            subscriberId_channelId: {
-                subscriberId: subscriberId,
-                channelId: channelId,
-            },
+          subscriberId_channelId: {
+            subscriberId,
+            channelId,
+          },
         },
-    });
+      });
 
-    if (existingSubscription) {
+      if (existingSubscription) {
         await prisma.subscription.delete({
-            where: { id: existingSubscription.id },
+          where: { id: existingSubscription.id },
         });
-
         return res.status(200).json({ message: "Unsubscribed successfully." });
-    } else {
+      } else {
         await prisma.subscription.create({
-            data: {
-               subscriberId: subscriberId, 
-                channelId: channelId,
-            },
+          data: { subscriberId, channelId },
         });
-
         return res.status(200).json({ message: "Subscribed successfully." });
+      }
+    } catch (error) {
+      console.error("Error toggling subscription:", error);
+      return res.status(500).json({ message: "Server error" });
     }
-} catch (error) {
-    console.error("Error toggling subscription:", error);
-    return res.status(500).json({ message: "internal server error" });
-}
-    }
+  }
 }
 
 export const subscribeController = new SubscribeController();
